3fd859ba3015ab17e8d22d0f1327b739bdb293ae
MikhailV89 <114851579+MikhailV89@users.noreply.github.com>
2023-04-27 12:38:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+num1 =  prompt('Whats number?');
+num2 =  prompt('Whats number?');
+num3 =  prompt('Whats number?');
+
+letsAverage = (parseInt(num1) + parseInt(num2) + parseInt(num3)) / 3;
+alert('Среднее арифметическое: ' + letsAverage + '')
